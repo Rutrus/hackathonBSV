38065bf589fd21bd47a3928351716e3de1513369
Lorenzo <rutrus@gmail.com>
2019-09-01 11:07:20 +0200

    Simulando ON/OFF

--- web/test.js
+++ web/test.js
@@ -1,15 +1,23 @@
-var segundos = 1;
+// setInterval(cambiaColor, 1000);
 
-var myVar = setInterval(myTimer, segundos*1000);
+function cambiaColor(response) {
+    if (response === undefined){
+        response = {}
+        response.ok = Math.round(Math.random())
+    }
 
-function myTimer() {
-    fetch("https://reqres.in/api/users?page=2")
-    .then(function(response) {
-        console.log(response);
-        if (response.ok) {
-            document.querySelector('.led').style.backgroundColor = "green";
-        }else{
-            document.querySelector('.led').style.backgroundColor = "red";
-        }
-    });
+    console.log(response);
+    if (response.ok) {
+        document.querySelector('.led').style.backgroundColor = "green";
+    }else{
+        document.querySelector('.led').style.backgroundColor = "red";
+    }   
 }
+
+/*
+const mbClient = new MoneyButtonClient("253e9c2eed154e4e9defa7bc50cdcbf6", "a97ce4413fc641f64c3a6470e5037ffb")
+await mbClient.logInAsApp()
+const payments = await mbClient.getOwnPayments()
+const data = payments.map (p => ({id: p.id, amount: p.amount, currency: p.currency}))
+console.log(data)
+*/
